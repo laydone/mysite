a1ed49f3cb5737790fd5dde36fb55ec00641ce5d
laydone <947714443@qq.com>
2019-01-17 15:51:11 +0800

    turn on debug

--- server/application/config.php
+++ server/application/config.php
@@ -15,9 +15,9 @@ return [
     // +----------------------------------------------------------------------
 
     // 应用调试模式
-    'app_debug'              => false,
+    'app_debug'              => true, /* TODO:上线改为false */
     // 应用Trace
-    'app_trace'              => false,
+	'app_trace'              => true, /* TODO:上线改为false */
     // 应用模式状态
     'app_status'             => '',
     // 是否支持多模块
